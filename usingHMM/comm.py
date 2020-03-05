22fea5157cf0430da7634d5100bd3e926a53c614
SorayaMitate <mitate0712@gmail.com>
2020-03-06 00:01:54 +0900

    modified line 103 in comm.py

--- usingHMM/comm.py
+++ usingHMM/comm.py
@@ -96,17 +96,15 @@ def comm(ITERATION, NUM_NODE, queue):
         #-----------------#
 
         #エリアの定義area('lat','lon','shadowing','pathloss','cluNum','shadowing_avg')
-        #area = SpacialColShadowing(const.DELTA_MESH, const.B, const.B\
-        #    ,const.SHADOWING_VAR, const.D_COR)
-        #area.to_csv('test.csv')
-        pl = pd.Series([PL(const.FC, calc_dist(row['X'],row['Y'],ap_list[0].x,ap_list[0].y))\
-            for i,row in area.iterrows()],name='PL')
-        area = pd.concat([area,pl],axis=1)
-        area['cluNum'] = -1
-        area['shadowing_avg'] = 0.0
-        area = calc_shadowingavg(area)
-        area.to_csv('sample.csv')
-        #area = pd.read_csv('sample.csv',index_col=0)
+        #pl = pd.Series([PL(const.FC, calc_dist(row['X'],row['Y'],ap_list[0].x,ap_list[0].y))\
+        #    for i,row in area.iterrows()],name='PL')
+        #area = pd.concat([area,pl],axis=1)
+        #area['cluNum'] = -1
+        #area['shadowing_avg'] = 0.0
+        #area = calc_shadowingavg(area)
+        #area.to_csv('sample.csv')
+
+        area = pd.read_csv('sample.csv',index_col=0)
         print(area)
 
         traj_list = randomTraj()
