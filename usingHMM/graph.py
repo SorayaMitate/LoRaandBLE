d90335886f252e8f2a32a4f10f80debc676b1a6b
SorayaMitate <mitate0712@gmail.com>
2020-05-19 21:05:35 +0900

    modified the number of ites

--- usingHMM/graph.py
+++ usingHMM/graph.py
@@ -21,7 +21,8 @@ from const import *
 #sns.heatmap(pivot,cmap = 'jet', linecolor='Black',square = True)
 #plt.show()
 
-path = 'C:\\Users\\soraya-PC\\code\\data\\result\\UsedSystem\\'
+path = 'C:\\Users\\soraya-PC\\code\\data\\result\\backup\\sr\\qos2\\'
+#path = 'C:\\Users\\soraya-PC\\code\\data\\result\\backup\\sr\\qos2_noshad\\'
 file_list = glob.glob(path+'*')
 
 sns.set_style("darkgrid")
@@ -75,89 +76,4 @@ for i in range(len(file_list)):
         ax[1,tmp].set_xlabel('')
         ax[1,tmp].legend(loc='upper right',fontsize=15)
 plt.show()
-
-'''
-for i in range(len(df)):
-    print(file_name[i])
-    df[i].plot.bar(fontsize=30,grid=True,legend=True,stacked=True,)
-    plt.xlabel('Distance between LoRa GW and Mobile Node [m]',fontsize=30)
-    plt.ylabel('Percentage of selected systems',fontsize=30)
-    plt.legend(loc='upper right',fontsize=30)
-    plt.show()
-    #plt.savefig(file_name[i])
-'''
-'''
-path = 'C:\\Users\\soraya-PC\\code\\results\\app\\5\\'
-path = 'C:\\Users\\soraya-PC\\Desktop\\ieice-eng\\results\\app\\'
-file_name = 'results.csv'
-#df = pd.read_csv(path+file_name,index_col='app')
-df = pd.read_csv(path+file_name)
-#df = df.drop('Unnamed: 0', axis=1)
-for k,row in df.iterrows():
-    df.at[k,'PER'] = 1.0-df.at[k,'PER'] 
-
-qos = [row['app'] for i,row in df.iterrows()]
-for i in range(len(qos)):
-    qos[i] = 'App.' + str(i+1)
-per = [row['PER'] for i,row in df.iterrows()]
-ene = [row['energy'] for i,row in df.iterrows()]
-delay = [row['delay'] for i,row in df.iterrows()]
-
-#fig, ax = plt.subplots(1, 3)
-plt.style.use('ggplot') 
-font = {'family' : 'meiryo'}
-matplotlib.rc('font', **font)
-
-#plt.subplot(1,3,1)
-plt.bar(qos,per,align="center")
-plt.xlabel('QoS Parameter with The Highest Priority',fontsize=30)
-plt.ylabel('Packet Error Rate [%]',fontsize=30)
-plt.tick_params(labelsize=30)
-plt.grid(True)
-plt.show()
-
-#plt.subplot(1,3,2)
-plt.bar(qos,delay,color="#1E7F00",align="center")
-plt.xlabel('QoS Parameter with The Highest Priority',fontsize=30)
-plt.ylabel('Current consumption [mA]',fontsize=30)
-plt.tick_params(labelsize=30)
-plt.grid(True)
-plt.show()
-
-#plt.subplot(1,3,3)
-plt.bar(qos,ene,color="#1E7F00",align="center")
-plt.xlabel('QoS Parameter with The Highest Priority',fontsize=30)
-plt.ylabel('Transmission Delay [s]',fontsize=30)
-plt.tick_params(labelsize=30)
-plt.grid(True)
-plt.show()
-'''
-
-def hist():
-    file_name = 'cluster_dist.csv'
-    df = pd.read_csv(file_name)
-    print(df.columns)
-    df = df.sort_values('dist')
-
-    leng = len(df)
-    BIN = 10
-    l = np.arange(100,210,BIN)
-    result = []
-    for b in l:
-        tmp = df[(b<=df['dist'])&(df['dist']<b+BIN)]
-        result.append(len(tmp)/leng)
-    print(result)
-
-    l = [i+5 for i in l]
-
-    fig = plt.figure()
-    plt.style.use('ggplot') 
-    font = {'family' : 'meiryo'}
-    matplotlib.rc('font', **font)
-    plt.bar(l,result,width=10)
-    #plt.xlabel("xlabel", fontsize=20)
-    #plt.ylabel("ylabel", fontsize=20)
-    #plt.legend(loc='upper right',fontsize=20)
-    plt.tick_params(labelsize=40)
-    plt.show()
 
