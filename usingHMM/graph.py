7b543d1c433802e4ae8d507db9208644242a0073
SorayaMitate <mitate0712@gmail.com>
2020-06-05 22:43:42 +0900

    modified graph

--- usingHMM/graph.py
+++ usingHMM/graph.py
@@ -123,9 +123,9 @@ def hist():
 
     DIST = np.arange(0,MAX_DIST,DIST_BIN)
 
-    MAC_CLU_DIST = 200
+    MAC_CLU_DIST = 150
     MIN_CLU_DIST = 100
-    CLU_DIST_BIN = 10
+    CLU_DIST_BIN = 5
     CLU_DIST = np.arange(MIN_CLU_DIST,MAC_CLU_DIST,CLU_DIST_BIN)
 
     df = pd.DataFrame(index=CLU_DIST,columns=DIST)
